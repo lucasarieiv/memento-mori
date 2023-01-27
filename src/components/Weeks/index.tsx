79958ade66477bee556c7749e0fff055675dfe41
Lucas Vieira <lucasvieira@luttos.com>
2023-01-27 19:30:10 -0300

    Finalizado lógica geral

--- src/components/Weeks/index.tsx
+++ src/components/Weeks/index.tsx
@@ -5,64 +5,65 @@ import {Week} from '../../interfaces/Week';
 import styles from './styles.module.css';
 
 
+
 interface WeeksProps {
   setYear: (year: number) => void;
   setWeek: (week: number) => void;
   setMainBoardWeeks: (weeks: any) => void;
-  receivedWeeks: Week[];
   weeksIndex: number;
-  allWeeks: {weeks: Week[]}[];
+  allWeeks: {weeks: Week[], middleWeeks: [number]}[];
+  cleanWeeks: {weeks: Week[]}[];
 }
 
-export default function Weeks({setYear, setWeek, setMainBoardWeeks, receivedWeeks, weeksIndex, allWeeks} : WeeksProps) {
-  const cleanWeeks: Week[] = [...receivedWeeks]
+export default function Weeks({setYear, setWeek, setMainBoardWeeks, weeksIndex, allWeeks, cleanWeeks} : WeeksProps) {
+  const cleanWeeksBoard: Week[] = [...cleanWeeks[weeksIndex].weeks]
   
-  const [weeks, setWeeks] = useState(() => {
-    return [...receivedWeeks]
-  })
-
   function handleClick(week: Week) {
     const year = (week.index + 1) / 26
-    console.log(allWeeks.length);
-    console.log(weeksIndex);
-    
-    
+
     setWeek(week.index)
     setYear(year / 2)
-
-    setWeeks(() => {
-      const newWeek = [...cleanWeeks];
-
-      newWeek.forEach(item => {
-        if (item.index <= week.index) {
-          item.isActive = true;
-        }
-      })
-
-      setMainBoardWeeks((prev: any) => {
-        const newBoardWeeks = [...prev]
-        newBoardWeeks[weeksIndex].weeks = [...newWeek]
-        return newBoardWeeks;
-      })
-
-      return newWeek;
-    })
+    console.log(week.index);
     
+    updateAllWeeksActives(week.index);
   }
 
-  function handleOver(index: number) {
-    console.log(index);
+  function updateAllWeeksActives(position: number) {
+    
+    setMainBoardWeeks(() => {
+      const newWeeks = [...cleanWeeks];
+      for (let count = 0; count <= weeksIndex; count++) {
+
+        newWeeks[count].weeks.forEach(item => {
+          if (item.index <= position) {
+            item.isActive = true;
+          }
+        })
+      }
+      
+      return newWeeks;
+    })
+  }
+
+  function middleWeeks(week: number) {
+    const hasFound = allWeeks[weeksIndex].middleWeeks.findIndex((element) => week === element)
+    
+    if (hasFound < 0) return false
+    return true
   }
 
   return (
     <div className={styles.container}>
       
-      {weeks.map((week) => {
+      {allWeeks[weeksIndex].weeks.map((week) => {
         return (
-          <span onClick={() => handleClick(week)} key={week.index}
-            className={`${styles.one_week} ${week.isActive ? styles.active : ''}`}></span>
+          <span 
+            onClick={() => handleClick(week)} key={week.index}
+            className={`${styles.one_week} ${middleWeeks(week.index) ? styles.red : ''} ${week.isActive ? styles.active : ''}`}>
+          </span>
         )
       })}
     </div>
   )
 }
+
