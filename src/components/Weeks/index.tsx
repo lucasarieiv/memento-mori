cbddca9e76220a900a0751622d3268aff42d244b
Lucas Vieira <lucasvieira@luttos.com>
2023-01-28 12:17:14 -0300

    Refatoramento

--- src/components/Weeks/index.tsx
+++ src/components/Weeks/index.tsx
@@ -9,57 +9,30 @@ import styles from './styles.module.css';
 interface WeeksProps {
   setYear: (year: number) => void;
   setWeek: (week: number) => void;
-  setMainBoardWeeks: (weeks: any) => void;
-  weeksIndex: number;
-  allWeeks: {weeks: Week[], middleWeeks: [number]}[];
-  cleanWeeks: {weeks: Week[]}[];
+  allWeeks: Week[];
 }
 
-export default function Weeks({setYear, setWeek, setMainBoardWeeks, weeksIndex, allWeeks, cleanWeeks} : WeeksProps) {
-  const cleanWeeksBoard: Week[] = [...cleanWeeks[weeksIndex].weeks]
+export default function Weeks({setYear, setWeek, allWeeks} : WeeksProps) {
+  const [weekNumber, setWeekNumber] = useState<number | null>(0);
   
   function handleClick(week: Week) {
     const year = (week.index + 1) / 26
+    setWeekNumber(week.index);
 
     setWeek(week.index)
     setYear(year / 2)
     console.log(week.index);
     
-    updateAllWeeksActives(week.index);
-  }
-
-  function updateAllWeeksActives(position: number) {
-    
-    setMainBoardWeeks(() => {
-      const newWeeks = [...cleanWeeks];
-      for (let count = 0; count <= weeksIndex; count++) {
-
-        newWeeks[count].weeks.forEach(item => {
-          if (item.index <= position) {
-            item.isActive = true;
-          }
-        })
-      }
-      
-      return newWeeks;
-    })
-  }
-
-  function middleWeeks(week: number) {
-    const hasFound = allWeeks[weeksIndex].middleWeeks.findIndex((element) => week === element)
-    
-    if (hasFound < 0) return false
-    return true
   }
 
   return (
     <div className={styles.container}>
       
-      {allWeeks[weeksIndex].weeks.map((week) => {
+      {allWeeks.map((week) => {
         return (
           <span 
             onClick={() => handleClick(week)} key={week.index}
-            className={`${styles.one_week} ${middleWeeks(week.index) ? styles.red : ''} ${week.isActive ? styles.active : ''}`}>
+            className={`${styles.one_week} ${week.index <= weekNumber  ? styles.active : ''}`}>
           </span>
         )
       })}
--- src/interfaces/Week.ts
+++ src/interfaces/Week.ts
@@ -1,4 +1,3 @@
 export interface Week {
   index: number,
-  isActive: boolean
 }
