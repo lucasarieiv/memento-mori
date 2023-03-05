a05fa26c80031eb9729fe6b0d52ed464ce756ca0
Lucas Vieira <lucasvieira.dev2020@gmail.com>
2023-03-05 16:55:07 -0300

    Renderizando semanas baseado no modo do app

--- src/components/Weeks/index.tsx
+++ src/components/Weeks/index.tsx
@@ -9,10 +9,10 @@ import { ModalWeekHabit } from '../ModalWeekHabit';
 
 export default function Weeks() {
   const {updateIndex, listWeeks, index, isAppCreateHabitsMode} = useAppContext();
-  console.log('Line 12 isAppNormalMode: ', isAppCreateHabitsMode);
 
   const [modalIsOpen, setModalIsOpen] = useState(false);
   const [weekNumber, setWeekNumber] = useState(0);
+  const [habitInterval, setHabitInterval] = useState(0);
 
   
   function loadBoardIndex() {
@@ -32,19 +32,41 @@ export default function Weeks() {
     setModalIsOpen(true);
   }
 
+  function changeHabitInterval(weekNumber: number) {
+    setHabitInterval(prev => {
+      console.log('Valor anterior', prev)
+      console.log('Novo valor', weekNumber)
+      return weekNumber
+    })
+  }
+
   return (
     <>
     {modalIsOpen && <ModalWeekHabit weekNumber={weekNumber} weekModalIsOpen={modalIsOpen} handleWeekModalIsOpen={setModalIsOpen} />}
 
-      <div className={styles.container}>
-        {listWeeks.map((week) => {
-          return (
-            <span 
-              onClick={() => handleClick(week)} key={week.index}
-              className={`${styles.one_week} ${isAppCreateHabitsMode && styles['-register-mode'] } ${week.index <= index  ? styles.active : ''}`}>
-            </span>
-          )
-        })}
+    <div className={styles.container}>
+        {!isAppCreateHabitsMode ? 
+          listWeeks.map((week) => {
+            return (
+              <span 
+                onClick={() => handleClick(week)} key={week.index}
+                className={`${styles.one_week} ${week.index <= index  ? styles.active : ''}`}>
+              </span>
+            )
+          })
+          :
+          listWeeks.map((week) => {
+            return (
+              <span 
+                key={week.index}
+                onClick={() => {
+                  changeHabitInterval(week.index);
+                }}
+                className={`${styles.one_week} ${styles['-register-mode']} ${week.index <= index  ? styles.active : ''}`}>
+              </span>
+            )
+          })
+        }
         
       </div>
     </>
