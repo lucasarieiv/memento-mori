9a79f024739770f33e6a5ad22fca7231fa3c9a71
Lucas Vieira <lucasvieira.dev2020@gmail.com>
2023-03-07 01:34:36 -0300

    Componente Check persistindo dados no localStorage

--- src/components/Check/index.tsx
+++ src/components/Check/index.tsx
@@ -1,13 +1,18 @@
 import { Habit } from "@/interfaces/Habit";
 import * as Checkbox from "@radix-ui/react-checkbox";
+import { CheckIcon } from '@radix-ui/react-icons';
 
 import styles from './styles.module.css';
+import { useAppContext } from "@/hooks/useAppContext";
 
 interface CheckboxProps {
-  habit: Habit
+  habit: Habit,
+  weekNumber: number
 }
 
-export function Check({habit}: CheckboxProps) {
+export function Check({habit, weekNumber}: CheckboxProps) {
+  const { habitListNumbersOfWeek, setHabitListNumbersOfWeek } = useAppContext();
+  const habitListWeek = habitListNumbersOfWeek.get(weekNumber) as Habit[][];
 
   function handleCompleted(habitId: string, weekDayNumber: number = 0) {
     const newHabitList = habitListWeek[weekDayNumber].map((habit) => {
@@ -16,22 +21,26 @@ export function Check({habit}: CheckboxProps) {
       }
       return habit;
     });
+
+    const newHabitListWeeks: Map<number, Habit[][]> = new Map([...habitListNumbersOfWeek]);
+
   
     habitListWeek[weekDayNumber] = newHabitList;
   
     newHabitListWeeks.set(weekNumber, habitListWeek);
     setHabitListNumbersOfWeek(newHabitListWeeks);
+    localStorage.setItem('@mementomori:weeksHabits', JSON.stringify([...newHabitListWeeks]))
   }
 
   return (
     <div
       className={styles.CheckboxContainer}
-      key={`${habit.id}`}
+      key={habit.id}
       onClick={() => handleCompleted(habit.id, habit.weekNumber)}
     >
-      <Checkbox.Root className={styles.CheckboxRoot}>
+      <Checkbox.Root className={styles.CheckboxRoot} checked={habit.isCompleted}>
         <Checkbox.Indicator className={styles.CheckboxIndicator}>
-          x
+          <CheckIcon />
         </Checkbox.Indicator>
       </Checkbox.Root>
       <p>{habit.text}</p>
